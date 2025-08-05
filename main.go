9991c37c041123616449cab7f147c29c3210bfc8
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-08-04 23:54:06 -0600

    feat: implement ParseFullArgument function to get argument-value

--- main.go
+++ main.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
+const OPTION_PREFIX = "-"
+const OPTION_SIZE = 3
+const OPTION_SIGN = "="
+
 var options map[string]string = map[string]string {
-	"--nme": "new-project", "--rte": "./", "--lng": "go", "--lic": "mit", "--dcs": "all",
+	OPTION_PREFIX + "nme": "new-project",
+	OPTION_PREFIX + "rte": "./",
+	OPTION_PREFIX + "lng": "go",
+	OPTION_PREFIX + "lic": "mit",
+	OPTION_PREFIX + "dcs": "all",
 }
 var languages map[string]string = map[string]string {
 	"python": ".py", "c": ".c", "golang": ".go", "lua": ".lua",
 	"java": ".java", "javascript": ".js",  "cpp": ".cpp", "r": ".r",
-	"": "",
 }
 var licences map[string]string = map[string]string {
 	"mit": "MIT", "apache": "APACHE",
@@ -19,8 +28,37 @@ var documents map[string]string = map[string]string {
 	"ignore": ".gitignore", "license": "LICENSE", "readme": "README", "all": "",
 }
 
+// ParseFullArgument parses and validates a command-line argument with the specified format.
+// Parameters:
+//   - arg: the full command-line argument to parse
+//   - prefix: expected argument prefix (e.g., "--")
+//   - sign: separator between argument and value (e.g., "=")
+//   - size: length of the argument name
+// Returns the argument name and its value if valid, empty strings otherwise.
+func ParseFullArgument(arg, prefix, sign string, size int) (string, string) {
+	expected_size := len(prefix) + size
+	if len(arg) < expected_size { return "", "" }
+	if !strings.HasPrefix(arg, prefix) { return "", "" }
+	assignator_idx := strings.Index(arg, sign)
+	if assignator_idx != expected_size { return "", "" }
+	return arg[:assignator_idx], arg[assignator_idx + 1:]
+}
+
+// ValidateArguments goes through each argument from os.Args, compares them with the
+// received map and assigns its value to the validated option.
+// Parameters:
+//   - opts: pointer to a map to compare and assing.
+// Returns nothing.
+func ValidateArguments(opts *map[string]string) {
+	arguments := os.Args[1:]
+	if len(arguments) < 2 { return }
+	for _, full_arg := range(arguments) {
+		arg, val := ParseFullArgument(full_arg, OPTION_PREFIX, OPTION_SIGN, OPTION_SIZE)
+		fmt.Printf("Argument: %s, Value: %s\n", arg, val)
+	}
+}
+
 func main() {
-	// TODO: Parse command-line argumnents.
 	// TODO: Validate options' values.
-	fmt.Println("Hello from main package!")
+	ValidateArguments(&options)
 }
