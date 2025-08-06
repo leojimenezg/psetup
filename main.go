fd90fc9b2697da4e551fe42f00db41aac5f70fb4
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-08-05 22:51:08 -0600

    style: update package name

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"github.com/leojimenezg/psetup/parsing"
+	"github.com/leojimenezg/psetup/argparse"
 )
 
 const OPTION_PREFIX = "-"
@@ -12,40 +11,32 @@ const OPTION_SIGN = "="
 
 func main() {
 	commandLineArgs := os.Args[1:]
-	arguments := parsing.Arguments {
-		&parsing.ArgumentConfig{
+	arguments := argparse.Arguments {
+		&argparse.ArgumentConfig{
 			Name: OPTION_PREFIX + "nme",
 			DefaultValue: "new-project",
-			AllowedValues: []string { parsing.ANY },
+			AllowedValues: []string { argparse.ANY },
 		},
-		&parsing.ArgumentConfig{
+		&argparse.ArgumentConfig{
 			Name: OPTION_PREFIX + "rte",
 			DefaultValue: "./",
-			AllowedValues: []string { parsing.ANY },
+			AllowedValues: []string { argparse.ANY },
 		},
-		&parsing.ArgumentConfig{
+		&argparse.ArgumentConfig{
 			Name: OPTION_PREFIX + "lng",
 			DefaultValue: "go",
 			AllowedValues: []string { "py", "c", "java", "go", "cpp", "lua", "js", "r" },
 		},
-		&parsing.ArgumentConfig{
+		&argparse.ArgumentConfig{
 			Name: OPTION_PREFIX + "lic",
 			DefaultValue: "mit",
 			AllowedValues: []string { "mit", "apache" },
 		},
-		&parsing.ArgumentConfig{
+		&argparse.ArgumentConfig{
 			Name: OPTION_PREFIX + "dcs",
 			DefaultValue: "all",
 			AllowedValues: []string { "all", "license", "ignore", "readme" },
 		},
 	}
-	fmt.Println("Before Processing")
-	for _, argConfig := range arguments {
-		fmt.Printf("%s = %s\n", argConfig.Name, argConfig.CurrentValue)
-	}
-	parsing.ProcessArguments(commandLineArgs, arguments, OPTION_PREFIX, OPTION_SIGN, OPTION_SIZE)
-	fmt.Println("After Processing")
-	for _, argConfig := range arguments {
-		fmt.Printf("%s = %s\n", argConfig.Name, argConfig.CurrentValue)
-	}
+	argparse.ProcessArguments(commandLineArgs, arguments, OPTION_PREFIX, OPTION_SIGN, OPTION_SIZE)
 }
