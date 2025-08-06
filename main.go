342199855a4e4ea672cf50fa3447289f8be396aa
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-08-05 20:20:58 -0600

    feat: complete first version of ProcessArguments function

--- main.go
+++ main.go
@@ -3,62 +3,49 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
+	"github.com/leojimenezg/psetup/parsing"
 )
 
 const OPTION_PREFIX = "-"
 const OPTION_SIZE = 3
 const OPTION_SIGN = "="
 
-var options map[string]string = map[string]string {
-	OPTION_PREFIX + "nme": "new-project",
-	OPTION_PREFIX + "rte": "./",
-	OPTION_PREFIX + "lng": "go",
-	OPTION_PREFIX + "lic": "mit",
-	OPTION_PREFIX + "dcs": "all",
-}
-var languages map[string]string = map[string]string {
-	"python": ".py", "c": ".c", "golang": ".go", "lua": ".lua",
-	"java": ".java", "javascript": ".js",  "cpp": ".cpp", "r": ".r",
-}
-var licences map[string]string = map[string]string {
-	"mit": "MIT", "apache": "APACHE",
-}
-var documents map[string]string = map[string]string {
-	"ignore": ".gitignore", "license": "LICENSE", "readme": "README", "all": "",
-}
-
-// ParseFullArgument parses and validates a command-line argument with the specified format.
-// Parameters:
-//   - arg: the full command-line argument to parse
-//   - prefix: expected argument prefix (e.g., "--")
-//   - sign: separator between argument and value (e.g., "=")
-//   - size: length of the argument name
-// Returns the argument name and its value if valid, empty strings otherwise.
-func ParseFullArgument(arg, prefix, sign string, size int) (string, string) {
-	expected_size := len(prefix) + size
-	if len(arg) < expected_size { return "", "" }
-	if !strings.HasPrefix(arg, prefix) { return "", "" }
-	assignator_idx := strings.Index(arg, sign)
-	if assignator_idx != expected_size { return "", "" }
-	return arg[:assignator_idx], arg[assignator_idx + 1:]
-}
-
-// ValidateArguments goes through each argument from os.Args, compares them with the
-// received map and assigns its value to the validated option.
-// Parameters:
-//   - opts: pointer to a map to compare and assing.
-// Returns nothing.
-func ValidateArguments(opts *map[string]string) {
-	arguments := os.Args[1:]
-	if len(arguments) < 2 { return }
-	for _, full_arg := range(arguments) {
-		arg, val := ParseFullArgument(full_arg, OPTION_PREFIX, OPTION_SIGN, OPTION_SIZE)
-		fmt.Printf("Argument: %s, Value: %s\n", arg, val)
-	}
-}
-
 func main() {
-	// TODO: Validate options' values.
-	ValidateArguments(&options)
+	commandLineArgs := os.Args[1:]
+	arguments := parsing.Arguments {
+		&parsing.ArgumentConfig{
+			Name: "-nme",
+			DefaultValue: "new-project",
+			AllowedValues: []string { "any" },
+		},
+		&parsing.ArgumentConfig{
+			Name: "-rte",
+			DefaultValue: "new-project",
+			AllowedValues: []string { "any" },
+		},
+		&parsing.ArgumentConfig{
+			Name: "-lng",
+			DefaultValue: "go",
+			AllowedValues: []string { "py", "c", "java", "go", "cpp", "lua", "js", "r" },
+		},
+		&parsing.ArgumentConfig{
+			Name: "-lic",
+			DefaultValue: "mit",
+			AllowedValues: []string { "mit", "apache" },
+		},
+		&parsing.ArgumentConfig{
+			Name: "-dcs",
+			DefaultValue: "all",
+			AllowedValues: []string { "all", "license", "ignore", "readme" },
+		},
+	}
+	fmt.Println("Before Processing")
+	for _, argConfig := range arguments {
+		fmt.Printf("%s = %s\n", argConfig.Name, argConfig.CurrentValue)
+	}
+	parsing.ProcessArguments(commandLineArgs, arguments, OPTION_PREFIX, OPTION_SIGN, OPTION_SIZE)
+	fmt.Println("After Processing")
+	for _, argConfig := range arguments {
+		fmt.Printf("%s = %s\n", argConfig.Name, argConfig.CurrentValue)
+	}
 }
