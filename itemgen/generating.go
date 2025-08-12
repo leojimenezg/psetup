54679278201054bfdd64a4ade7d63819e4417187
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-08-11 20:49:24 -0600

    feat: functions to work with embedded filesystem from "embed" package

--- itemgen/generating.go
+++ itemgen/generating.go
@@ -3,6 +3,7 @@ package itemgen
 import (
 	"fmt"
 	"os"
+	"embed"
 	"path/filepath"
 )
 
@@ -57,38 +58,72 @@ type ItemConfig struct {
 	Type ItemType
 	CreationPath string
 	TemplatePath string
+	TemplateContent []byte
 }
 
-type Configs []ItemConfig
+type Items []ItemConfig
+
+func createFullPath(path, name, ext string) string {
+	var fullPath string
+	if ext == "" { 
+		fullPath = filepath.Join(path, name)
+	} else if ext[0] != '.' {
+		fullPath = filepath.Join(path, name + "." + ext)
+	} else {
+		fullPath = filepath.Join(path, name + ext)
+	}
+	return fullPath
+}
 
 // CreateFile creates a new file based on the provided configuration.
 // The function handles extension formatting automatically, adding a dot prefix if not present.
-// If a template path is specified, the file content is read from the template file;
-// otherwise, an empty file is created.
+// Content priority: If template content is provided, it is used directly. If not, but a template 
+// path is specified, the file content is read from the template file. Otherwise, an empty file is created.
 // Parameters:
-//   - item: ItemConfig struct containing file name, extension, type, creation path, and template path
+//   - item: ItemConfig struct containing all necessary configurations
 // Returns nil if successful, or an error of type InvalidTypeError, TemplateError, or CreationError.
 func CreateFile(item ItemConfig) error {
 	if item.Type != FILE { return InvalidTypeError{ Type: item.Type } }
-	var fullPath string
-	if item.Extension == "" { 
-		fullPath = filepath.Join(item.CreationPath, item.Name)
-	} else if item.Extension[0] != '.' {
-		fullPath = filepath.Join(item.CreationPath, item.Name + "." + item.Extension)
-	} else {
-		fullPath = filepath.Join(item.CreationPath, item.Name + item.Extension)
+	fullPath := createFullPath(item.CreationPath, item.Name, item.Extension)
+	if item.TemplateContent != nil {
+		errFile := os.WriteFile(fullPath, item.TemplateContent, 0644)
+		if errFile != nil { return CreationError{ Name: item.Name, Path: fullPath, Err: errFile } }
+		return nil
 	}
-	var fileContent[]byte
+	var template[]byte = nil
 	if item.TemplatePath != "" { 
-		template, errTemplate := os.ReadFile(item.TemplatePath)
-		if errTemplate != nil {
-			return TemplateError{ Path: item.TemplatePath, Err: errTemplate }
-		}
-		fileContent = template
-	} else {
-		fileContent = nil
+		data, errTemplate := os.ReadFile(item.TemplatePath)
+		if errTemplate != nil { return TemplateError{ Path: item.TemplatePath, Err: errTemplate } }
+		template = data
 	}
-	errFile := os.WriteFile(fullPath, fileContent, 0644)  // Filemode: permissions and attributes.
+	errFile := os.WriteFile(fullPath, template, 0644)  // Filemode: permissions and attributes.
+	if errFile != nil { return CreationError{ Name: item.Name, Path: fullPath, Err: errFile } }
+	return nil
+}
+
+// CreateFileEmbed creates a new file based on the provided configuration using an embedded filesystem.
+// The function handles extension formatting automatically, adding a dot prefix if not present.
+// Content priority: If template content is provided, it is used directly. If not, but a template 
+// path is specified, the file content is read from the embedded filesystem. Otherwise, an empty file is created.
+// Parameters:
+//   - item: ItemConfig struct containing all necessary configurations
+//   - fs: Pointer to embed.FS containing the template files
+// Returns nil if successful, or an error of type InvalidTypeError, TemplateError, or CreationError.
+func CreateFileEmbed(item ItemConfig, fs *embed.FS) error {
+	if item.Type != FILE { return InvalidTypeError{ Type: item.Type } }
+	fullPath := createFullPath(item.CreationPath, item.Name, item.Extension)
+	if item.TemplateContent != nil {
+		errFile := os.WriteFile(fullPath, item.TemplateContent, 0644)
+		if errFile != nil { return CreationError{ Name: item.Name, Path: fullPath, Err: errFile } }
+		return nil
+	}
+	var template []byte = nil
+	if item.TemplatePath != "" {
+		data, errTemplate := fs.ReadFile(item.TemplatePath)
+		if errTemplate != nil { return TemplateError{ Path: item.TemplatePath, Err: errTemplate } }
+		template = data
+	}
+	errFile := os.WriteFile(fullPath, template, 0644)
 	if errFile != nil { return CreationError{ Name: item.Name, Path: fullPath, Err: errFile } }
 	return nil
 }
@@ -106,25 +141,45 @@ func CreateDirectory(item ItemConfig) error {
 	return nil
 }
 
-// CreateItems iterates through a Configs slice and creates each item based on its type.
+// CreateItems iterates through an Items slice and creates each item based on its type.
 // The function processes all items regardless of individual failures, collecting any errors encountered.
 // Parameters:
-//   - items: Configs slice containing all ItemConfig structs to create
+//   - items: Items slice containing all ItemConfig structs to create
 // Returns nil if everything is successful, or a slice of all errors encountered.
-func CreateItems(items Configs) []error {
+func CreateItems(items Items) []error {
 	var errors []error
 	for _, item := range items {
 		switch item.Type {
 		case FILE:
 			errFile := CreateFile(item)
-			if errFile != nil {
-				errors = append(errors, errFile)
-			}
+			if errFile != nil { errors = append(errors, errFile) }
 		case DIR:
 			errDir := CreateDirectory(item)
-			if errDir != nil {
-				errors = append(errors, errDir)
-			}
+			if errDir != nil { errors = append(errors, errDir) }
+		default:
+			errors = append(errors, InvalidTypeError{ Type: item.Type })
+		}
+	}
+	if len(errors) == 0 { return nil }
+	return errors
+}
+
+// CreateItemsEmbed iterates through an Items slice and creates each item based on its type using an embedded filesystem.
+// The function processes all items regardless of individual failures, collecting any errors encountered.
+// Parameters:
+//   - items: Items slice containing all ItemConfig structs to create
+//   - fs: Pointer to embed.FS containing the template files for FILE items
+// Returns nil if everything is successful, or a slice of all errors encountered.
+func CreateItemsEmbed(items Items, fs *embed.FS) []error {
+	var errors []error
+	for _, item := range items {
+		switch item.Type {
+		case FILE:
+			errFile := CreateFileEmbed(item, fs)
+			if errFile != nil { errors = append(errors, errFile) }
+		case DIR:
+			errDir := CreateDirectory(item)
+			if errDir != nil { errors = append(errors, errDir) }
 		default:
 			errors = append(errors, InvalidTypeError{ Type: item.Type })
 		}
